522977bc2d0a6fe5ef33ae703a6064fca85ae292
Ayudesee <sharaevvlad@mail.ru>
2015-01-06 02:19:48 +0300

    FirstCommit1

---
+++ BlogApp/Views/Models/RecentPostItemModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlogApp.Views.Models
+{
+    public class RecentPostItemModel
+    {
+        public string text { get; set; }
+        public string URL { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
